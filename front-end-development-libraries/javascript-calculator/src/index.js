da92268cef2a71c08af4b52267c6072bd7f8fb21
thrivingproject <christianisaman@outlook.com>
2022-09-28 14:53:29 -0400

    feat: created App

--- front-end-development-libraries/javascript-calculator/src/index.js
+++ front-end-development-libraries/javascript-calculator/src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './site.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+  
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>
 );
 
