8148a162ffe4b02a078581a7ad52274f4f813958
thrivingproject <christianisaman@outlook.com>
2022-09-29 16:40:26 -0400

    fix: reduced redundancy

--- front-end-development-libraries/javascript-calculator/src/index.js
+++ front-end-development-libraries/javascript-calculator/src/index.js
@@ -4,42 +4,40 @@ import './site.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  // componentDidMount() {
-  //   document.addEventListener('keydown', this.handleKeyPress);
-  // }
-
-  // componentWillUnmount() {
-  //   document.removeEventListener('keydown', this.handleKeyPress);
-  // }
-
-  handleKeyPress = () => {
-    this.props.clicked(this.props.label)
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleKeyPress} id={this.props.id}>{this.props.label}</button>
-    )
-  }
+const Button = (props) => {
+  return (
+    <button onClick={() => props.clicked(props.label)} id={props.id}>{props.label}</button>
+  )
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      display: ''
+      display: '',
+      lastClicked: ''
     }
   }
 
   updateDisplay = (label) => {
-    this.setState((state, props) => {
-      return {display: state.display.concat(label)}
-    })
+    const display = this.state.display
+    const prevVal = display.charAt(display.length - 1)
+
+    // Check if decimal already present
+    if (display.includes('.') && label === '.') {
+      return
+    }
+
+    // Check for consecutive operators
+    if (operators.includes(prevVal) && operators.includes(label)) {
+      if (prevVal !== label) {
+
+      }
+    }
+
+    this.setState((state, props) => ({
+      display: state.display + label,
+    }))
   }
 
   clearDisplay = () => {
@@ -56,6 +54,7 @@ class App extends React.Component {
     return (
       <div id='calculator'>
         <div id="display"><h1>{this.state.display}</h1></div>
+
         <Button clicked={this.updateDisplay} label={0} id='zero' />
         <Button clicked={this.updateDisplay} label={1} id='one' />
         <Button clicked={this.updateDisplay} label={2} id='two' />
@@ -66,18 +65,21 @@ class App extends React.Component {
         <Button clicked={this.updateDisplay} label={7} id='seven' />
         <Button clicked={this.updateDisplay} label={8} id='eight' />
         <Button clicked={this.updateDisplay} label={9} id='nine' />
+
         <Button clicked={this.updateDisplay} label='+' id='add' />
         <Button clicked={this.updateDisplay} label='-' id='subtract' />
         <Button clicked={this.updateDisplay} label='/' id='divide' />
         <Button clicked={this.updateDisplay} label='*' id='multiply' />
+
         <Button clicked={this.updateDisplay} label='.' id='decimal' />
         <Button clicked={this.clearDisplay} label='clear' id='clear' />
         <button clicked={this.calculate} id="equals">=</button>
       </div>
     )
   }
-
 }
+
+const operators = ['+', '/', '*']
 
 root.render(
   <React.StrictMode>
