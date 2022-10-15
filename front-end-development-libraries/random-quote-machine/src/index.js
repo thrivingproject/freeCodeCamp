e04f55281117e1b657b00983ecec590eab05ec8f
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-10-15 14:55:47 -0400

    fix: deleted comment

--- front-end-development-libraries/random-quote-machine/src/index.js
+++ front-end-development-libraries/random-quote-machine/src/index.js
@@ -60,55 +60,6 @@ const App = () => {
     );
 }
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             quotesArray: [],
-//             quote: '',
-//             author: ''
-//         }
-//     }
-
-//     componentDidMount() {
-//         fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-//             .then(response => response.json())
-//             .then(data => {
-//                 this.setState({
-//                     quotesArray: data.quotes
-//                 })
-//             })
-//             .then(() => this.newQuote())
-//     }
-
-//     newQuote = () => {
-//         let r = Math.floor(Math.random() * 103)
-//         this.setState((state) => ({
-//             quote: state.quotesArray[r].quote,
-//             author: state.quotesArray[r].author
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <div className='gridcontainer'>
-//                 <section id='quote-box' className='gridcontainer'>
-//                     <Text quote={this.state.quote} />
-//                     <Author author={this.state.author} />
-//                     <div id='buttons'>
-//                         <NewQuote handler={this.newQuote} />
-//                         <a
-//                             id='tweet-quote'
-//                             href='https://www.twitter.com/intent/tweet'>
-//                             <button>Tweet</button>
-//                         </a>
-//                     </div>
-//                 </section>
-//             </div>
-//         );
-//     }
-// }
-
 root.render(
     <React.StrictMode>
         <App />
