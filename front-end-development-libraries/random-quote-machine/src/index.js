82fc8e6c43d1095df6a1dda7776c7a19f33e1097
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-10-15 13:33:37 -0400

    feat: converted class components to function

--- front-end-development-libraries/random-quote-machine/src/index.js
+++ front-end-development-libraries/random-quote-machine/src/index.js
@@ -4,85 +4,58 @@ import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// class Text extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-
-//   render() {
-//     return (
-//       <p className='text'>{this.props.quote}</p>
-//     )
-//   }
-// }
-
 const Text = (props) => <p className='text'>{props.quote}</p>
 
-class Author extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Author = (props) => <p className='author'>{props.author}</p>
 
-  render() {
-    return (
-      <p className='author'>{this.props.author}</p>
-    )
-  }
-}
-
-class NewQuote extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <button onClick={this.props.handler} className='button'>New Quote</button>
-    )
-  }
+const NewQuote = (props) => {
+    return <button
+        onClick={props.handler}
+        className='button'>
+        New Quote</button>
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  componentDidMount() {
-    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          quotesArray: data.quotes
-        })
-      })
-      .then(() => this.newQuote())
-  }
+    componentDidMount() {
+        fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    quotesArray: data.quotes
+                })
+            })
+            .then(() => this.newQuote())
+    }
 
-  newQuote = () => {
-    let r = Math.floor(Math.random() * 103)
-    this.setState((state) => ({
-      quote: state.quotesArray[r].quote,
-      author: state.quotesArray[r].author
-    }))
-  }
+    newQuote = () => {
+        let r = Math.floor(Math.random() * 103)
+        this.setState((state) => ({
+            quote: state.quotesArray[r].quote,
+            author: state.quotesArray[r].author
+        }))
+    }
 
-  render() {
-    return (
-      <div className='gridcontainer'>
-        <section id='quote-box' className='gridcontainer'>
-          <div id='text'><Text quote={this.state.quote} /></div>
-          <div id='author'><Author author={this.state.author} /></div>
-          <div id='new-quote'><NewQuote handler={this.newQuote} /></div>
-          <a id='tweet-quote' href='https://www.twitter.com/intent/tweet'><button>Tweet</button></a>
-        </section>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='gridcontainer'>
+                <section id='quote-box' className='gridcontainer'>
+                    <div id='text'><Text quote={this.state.quote} /></div>
+                    <div id='author'><Author author={this.state.author} /></div>
+                    <div id='new-quote'><NewQuote handler={this.newQuote} /></div>
+                    <a id='tweet-quote' href='https://www.twitter.com/intent/tweet'><button>Tweet</button></a>
+                </section>
+            </div>
+        );
+    }
 }
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
