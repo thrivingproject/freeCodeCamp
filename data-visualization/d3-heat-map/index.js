5a5a85f5e5637e329f44c0d13fbc0a57489bef9f
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-10-16 14:32:26 -0400

    feat: month zero-indexed

--- data-visualization/d3-heat-map/index.js
+++ data-visualization/d3-heat-map/index.js
@@ -15,46 +15,59 @@ fetch(url)
     .then(data => {
         const base = data.baseTemperature
         const variance = data.monthlyVariance
+        variance.forEach(d => d.month -= 1)
 
-        const xScale = d3.scaleLinear()
+        // X-scale and x-axis
+        const xScale = d3
+            .scaleLinear()
             .range([paddingLeft, width - padding])
             .domain([1753, 2015])
         const xAxis = d3.axisBottom(xScale)
         svg.append('g')
             .attr('id', 'x-axis')
             .call(xAxis)
-            .attr('transform', `translate(0,${height - padding})`)
+            .attr('transform', `translate(0, ${height - padding})`)
 
-        const yScale = d3.scaleLinear()
-            .range([height - padding, padding])
-            .domain([13, 1])
-        const yAxis = d3.axisLeft(yScale)
+        // Y-scale and y-axis
+        const yScale = d3
+            .scaleBand()
+            .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+            .rangeRound([padding, height - padding])
+        const yAxis = d3
+            .axisLeft(yScale)
+            .tickFormat(month => {
+                let date = new Date(0)
+                date.setUTCMonth(month)
+                return d3.timeFormat('%B')(date)
+            })
         svg.append('g')
             .attr('id', 'y-axis')
             .call(yAxis)
             .attr('transform', `translate(${paddingLeft}, 0)`)
 
+        // Graph
         svg.selectAll('rect')
             .data(variance)
             .enter()
             .append('rect')
             .attr('class', 'cell')
-            .attr('data-month', d => yScale(d.month))
-            .attr('data-year', d => xScale(d.year))
+            .attr('data-month', d => d.month)
+            .attr('data-year', d => d.year)
             .attr('data-temp', d => base + d.variance)
             .attr('x', d => xScale(d.year))
             .attr('y', d => yScale(d.month))
             .attr('width', width / (2015 - 1753))
-            .attr('height', (height - padding) / 13)
+            .attr('height', d => yScale.bandwidth(d.month))
             .attr('fill', d => {
-                if (d.variance < -3) {return '#001d38'}
-                else if (d.variance < -2) {return '#00498d'}
-                else if (d.variance < -1) {return '#0084ff'}
-                else if (d.variance < 0) {return '#0fffff'}
-                else if (d.variance < 1) {return '#fffff0'}
-                else if (d.variance < 2) {return '#ffed00'}
-                else if (d.variance < 3) {return '#FFA500'}
-                else {return '#FF0C00'}
+                if (d.variance < -4.76) {return '#4575B4'}
+                else if (d.variance < -3.66) {return '#74ADD1'}
+                else if (d.variance < -2.56) {return '#ABD9E9'}
+                else if (d.variance < -1.46) {return '#E0F3F8'}
+                else if (d.variance < -.36) {return '#FFFFBF'}
+                else if (d.variance < .84) {return '#FEE090'}
+                else if (d.variance < 1.94) {return '#FDAE61'}
+                else if (d.variance < 3.04) {return '#F46D43'}
+                else {return '#D73027'}
             })
     })
 
