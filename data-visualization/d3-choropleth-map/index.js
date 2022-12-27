34474f829bbfa0d051fc983a47a793c7a028262f
Christian Isaman <christianisaman@outlook.com>
2022-12-27 09:57:32 -0400

    progress

--- data-visualization/d3-choropleth-map/index.js
+++ data-visualization/d3-choropleth-map/index.js
@@ -1,6 +1,31 @@
-const [height, width, padding] = [540, 720, 30]
+const [width, height, padding] = [960, 600, 30]
+const colors = ['#EE5533', '#ef6749', '#f17a60', '#f38d77']
 
 const svg = d3
     .select('#plot')
     .attr('height', height)
     .attr('width', width)
+
+edURL =
+    'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+mapURL =
+    'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+
+async function create() {
+    const data = await Promise.all([d3.json(edURL), d3.json(mapURL)])
+    const [edData, mapData] = [data[0], data[1]]
+    const path = d3.geoPath()
+    const map = topojson.feature(mapData, mapData.objects.counties).features
+
+    svg
+        .append('g')
+        .selectAll('path')
+        .data(map)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('class', 'county')
+
+}
+
+create()
